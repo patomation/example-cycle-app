ac59e38bdb9a7750a35948ad4839d31b3e6a7798
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2018-09-15 23:20:59 +0700

    chore: replace parcel with webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const { resolve } = require('path')
+
+const distPath = resolve(__dirname, 'dist');
+
+module.exports = {
+  context: __dirname,
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.ts',
+  output: {
+    path: distPath,
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader' }
+    ]
+  },
+  devServer: {
+    contentBase: distPath
+  }
+}
